28f8505a364139adae7c1461979070770845b898
unknown <eliser@nealanalytics.com>
2023-04-04 20:28:06 -0700

    added a page where people can add their ratings

--- BlissfulBorders/app.py
+++ BlissfulBorders/app.py
@@ -10,10 +10,13 @@ def index():
 
 @app.route("/preferences")
 def preferences():
-    return render_template(
-        "preferences.html", text="Your top recommendation was Tokyo!"
-    )
+    return render_template("preferences.html")
+
+
+@app.route("/add-rating")
+def add_rating():
+    return render_template("add-rating.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="http://127.0.0.1", port=5000)
